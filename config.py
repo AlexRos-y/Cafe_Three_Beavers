03de8771a1a0faa821cb1c5ebaf39de5e1fd8d12
Max Frolov <Maxwell939@users.noreply.github.com>
2026-05-10 15:53:55 +0300

    fix

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'three-beavers-secret-key-2026'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///cafe.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    YANDEX_MAPS_API_KEY = os.getenv("7f9cd07c-0f26-4523-9f72-e4b7542a45d5")
